9df13eae76110a1fee0ea1ba0fea0a2ac1f1899d
swapnesh <swapnesha839@gmail.com>
2024-08-30 18:24:02 +0530

    Revert "Merge branch 'main' into dev"
    This reverts commit a58025cc210bd30eadffcc6a24994d3a1bf54564, reversing
    changes made to 2685ddb61df76e1ce6964b356060f40b789c59c9.

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,6 @@
-
 import Background from "@/components/globalbackground/Background";
 import Herosection from "@/components/Home/Herosection";
 import Technologies from "@/components/Home/Technologies/Technologies";
-
 import Image from "next/image";
 
 export default function Home() {
